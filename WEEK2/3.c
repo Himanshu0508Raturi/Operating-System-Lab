545cc75bbbe1e66980ebb28d0b7364bf571c3581
Himanshu_Raturi <raturihimanshu077@gmail.com>
2025-07-30 22:20:07 +0530

    created 3.c

---
+++ WEEK2/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    FILE *file1, *file2;
+    char line1[100], line2[100];
+    file1 = fopen("my_details.txt", "w");
+    fprintf(file1, "Name: Himanshu Raturi\nRoll: 31\nCity: Rishikesh\n");
+    fclose(file1);
+    file2 = fopen("friend_details.txt", "w");
+    fprintf(file2, "Name: Akhil Bhatt\nRoll: 30\nCity: Dehradun\n");
+    fclose(file2);
+    file1 = fopen("my_details.txt", "r");
+    file2 = fopen("friend_details.txt", "r");
+    while (fgets(line1, sizeof(line1), file1))
+    {
+        rewind(file2);
+        while (fgets(line2, sizeof(line2), file2))
+        {
+            if (strcmp(line1, line2) == 0)
+            {
+                printf("Common line: %s", line1);
+            }
+        }
+    }
+    fclose(file1);
+    fclose(file2);
+    return 0;
+}
