66f3d479a1dc2e6ec310e72b799790be30da5c10
Himanshu_Raturi <raturihimanshu077@gmail.com>
2025-10-29 19:32:49 +0530

    29/10/25

--- WEEK5/a.c
+++ WEEK5/a.c
@@ -1,85 +1,218 @@
+// #include <stdio.h>
+// struct Process
+// {
+//     int A, B, C; // resources
+// };
+
+// int main()
+// {
+//     int n, A, B, C;
+//     printf("Enter the Number of Process - ");
+//     scanf("%d", &n);
+
+//     struct Process allocated[n];
+//     struct Process require[n];
+//     struct Process Need[n];
+//     int isExecute[n];
+//     int ans[n];
+
+//     for (int i = 0; i < n; i++)
+//         isExecute[i] = 0;
+//     int al_a = 0;
+//     int al_b = 0;
+//     int al_c = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("Enter the Allocated and Required Resources of P%d - ", i);
+//         scanf("%d%d%d", &allocated[i].A, &allocated[i].B, &allocated[i].C);
+//         scanf("%d%d%d", &require[i].A, &require[i].B, &require[i].C);
+
+//         al_a += allocated[i].A;
+//         al_b += allocated[i].B;
+//         al_c += allocated[i].C;
+
+//         Need[i].A = require[i].A - allocated[i].A;
+//         Need[i].B = require[i].B - allocated[i].B;
+//         Need[i].C = require[i].C - allocated[i].C;
+//     }
+
+//     printf("Enter the Aavilable Resources = ");
+//     scanf("%d%d%d", &A, &B, &C);
+//     A -= al_a;
+//     B -= al_b;
+//     C -= al_c;
+
+//     int idx = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (isExecute[i] == 0)
+//         {
+//             for (int j = 0; j < n; j++)
+//             {
+//                 if (isExecute[j] == 0)
+//                 {
+//                     if (Need[j].A <= A && Need[j].B <= B && Need[j].C <= C)
+//                     {
+//                         A += allocated[j].A;
+//                         B += allocated[j].B;
+//                         C += allocated[j].C;
+
+//                         ans[idx] = j;
+//                         idx++;
+//                         isExecute[j] = 1;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (isExecute[i] == 0)
+//         {
+//             printf("No Safe Sequence is Possible");
+//             return 0;
+//         }
+//     }
+
+//     printf("The Safe Sequence is - ");
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         printf("P%d ->", ans[i]);
+//     }
+//     printf("P%d", ans[n - 1]);
+//     return 0;
+// }
 #include <stdio.h>
-struct Process
+
+// Structure to represent resources (3 types: a, b, c)
+typedef struct Resource
 {
-    int A, B, C; // resources
-};
+    int a, b, c;
+} Resource;
 
 int main()
 {
-    int n, A, B, C;
-    printf("Enter the Number of Process - ");
-    scanf("%d", &n);
+    int p, r;
 
-    struct Process allocated[n];
-    struct Process require[n];
-    struct Process Need[n];
-    int isExecute[n];
-    int ans[n];
+    printf("Enter number of processes : ");
+    scanf("%d", &p);
 
-    for (int i = 0; i < n; i++)
-        isExecute[i] = 0;
-    int al_a = 0;
-    int al_b = 0;
-    int al_c = 0;
-    for (int i = 0; i < n; i++)
+    printf("Enter number of resources : ");
+    scanf("%d", &r);
+
+    if (r != 3)
     {
-        printf("Enter the Allocated and Required Resources of P%d - ", i);
-        scanf("%d%d%d", &allocated[i].A, &allocated[i].B, &allocated[i].C);
-        scanf("%d%d%d", &require[i].A, &require[i].B, &require[i].C);
-
-        al_a += allocated[i].A;
-        al_b += allocated[i].B;
-        al_c += allocated[i].C;
-
-        Need[i].A = require[i].A - allocated[i].A;
-        Need[i].B = require[i].B - allocated[i].B;
-        Need[i].C = require[i].C - allocated[i].C;
+        printf("This program only supports 3 resources (a, b, c)\n");
+        return 0;
     }
 
-    printf("Enter the Aavilable Resources = ");
-    scanf("%d%d%d", &A, &B, &C);
-    A -= al_a;
-    B -= al_b;
-    C -= al_c;
+    Resource max_need[p], already_allocated[p], remaining_need[p];
+    Resource total, avail;
 
-    int idx = 0;
-    for (int i = 0; i < n; i++)
+    // Input maximum requirement matrix
+    printf("Enter maximum requirement : \n");
+    for (int i = 0; i < p; i++)
     {
-        if (isExecute[i] == 0)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (isExecute[j] == 0)
-                {
-                    if (Need[j].A <= A && Need[j].B <= B && Need[j].C <= C)
-                    {
-                        A += allocated[j].A;
-                        B += allocated[j].B;
-                        C += allocated[j].C;
+        scanf("%d %d %d", &max_need[i].a, &max_need[i].b, &max_need[i].c);
+    }
 
-                        ans[idx] = j;
-                        idx++;
-                        isExecute[j] = 1;
-                    }
+    // Input allocated matrix
+    printf("Enter allocated matrix : \n");
+    for (int i = 0; i < p; i++)
+    {
+        scanf("%d %d %d", &already_allocated[i].a, &already_allocated[i].b, &already_allocated[i].c);
+    }
+
+    // Input total resources
+    printf("Resource Vector : ");
+    scanf("%d %d %d", &total.a, &total.b, &total.c);
+
+    // Calculate Available = Total - Sum(Alloc)
+    avail.a = total.a;
+    avail.b = total.b;
+    avail.c = total.c;
+    for (int i = 0; i < p; i++)
+    {
+        avail.a -= already_allocated[i].a;
+        avail.b -= already_allocated[i].b;
+        avail.c -= already_allocated[i].c;
+    }
+
+    // Calculate Need = Max - Alloc
+    for (int i = 0; i < p; i++)
+    {
+        remaining_need[i].a = max_need[i].a - already_allocated[i].a;
+        remaining_need[i].b = max_need[i].b - already_allocated[i].b;
+        remaining_need[i].c = max_need[i].c - already_allocated[i].c;
+    }
+
+    // Banker's Algorithm
+    int finish[p];
+    int safeSeq[p];
+    for (int i = 0; i < p; i++)
+        finish[i] = 0;
+    int count = 0;
+    int deadlock[p];
+    for(int i = 0 ; i < p ; i++)
+    {
+        deadlock[i] = 0;
+    }
+
+    while (count < p)
+    {
+        int found = 0;
+        for (int i = 0; i < p; i++)
+        {
+            if (!finish[i])
+            {
+                // Check if need <= avail
+                if (remaining_need[i].a <= avail.a &&
+                    remaining_need[i].b <= avail.b &&
+                    remaining_need[i].c <= avail.c)
+                {
+
+                    // Pretend to allocate
+                    avail.a += already_allocated[i].a;
+                    avail.b += already_allocated[i].b;
+                    avail.c += already_allocated[i].c;
+
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                    deadlock[i] = 1;
                 }
             }
         }
+        if (!found)
+            break; // no process can proceed
     }
 
-    for (int i = 0; i < n; i++)
+    // Output
+    if (count < p)
     {
-        if (isExecute[i] == 0)
+        printf("System is in UNSAFE state. No safe sequence exists.\n");
+        printf("Deadlock occur due to process: ");
+        for(int i = 0 ; i < p ; i++)
         {
-            printf("No Safe Sequence is Possible");
-            return 0;
+            if(!deadlock[i])
+            {
+                printf("P%D", i);
+            }
         }
     }
-
-    printf("The Safe Sequence is - ");
-    for (int i = 0; i < n - 1; i++)
+    else
     {
-        printf("P%d ->", ans[i]);
+        printf("Request can be fulfilled\n");
+        printf("Safe Sequence : ");
+        for (int i = 0; i < p; i++)
+        {
+            printf("P%d", safeSeq[i]);
+            if (i != p - 1)
+                printf(" ");
+        }
+        printf("\n");
     }
-    printf("P%d", ans[n - 1]);
+
     return 0;
 }
---
+++ WEEK7/1.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+
+void enqueue(int q[], int *front, int *rear, int v, int n)
+{
+    if (*rear == n - 1)
+    {
+        printf("Queue is full\n");
+        return;
+    }
+    else
+    {
+        if (*front == -1)
+        {
+            *front = 0;
+        }
+        q[++(*rear)] = v;
+    }
+}
+
+int dequeue(int q[], int *front, int *rear)
+{
+    int v;
+    if (*front == -1)
+    {
+        printf("Queue is empty\n");
+        return -1;
+    }
+    else
+    {
+        v = q[*front];
+        (*front)++;
+        if (*front > *rear)
+        {
+            *front = -1;
+            *rear = -1;
+        }
+    }
+    return v;
+}
+
+int isPresent(int q[], int front, int rear, int page)
+{
+    if (front == -1)
+        return 0;
+    for (int i = front; i <= rear; i++)
+    {
+        if (q[i] == page)
+            return 1;
+    }
+    return 0;
+}
+
+void display(int q[], int front, int rear)
+{
+    if (front == -1)
+    {
+        printf("Queue is empty\n");
+        return;
+    }
+    for (int i = front; i <= rear; i++)
+    {
+        printf("%d ", q[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int n, m;
+    printf("Enter number of frames: ");
+    scanf("%d", &n);
+    printf("Enter number of page requests: ");
+    scanf("%d", &m);
+
+    int pages[m];
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d", &pages[i]);
+    }
+
+    int q[20];
+    int front = -1, rear = -1;
+    int pageFaults = 0;
+    for (int i = 0; i < m; i++)
+    {
+        int page = pages[i];
+        printf("Request for page %d -> ", page);
+
+        // If page not in queue
+        if (!isPresent(q, front, rear, page))
+        {
+            pageFaults++;
+
+            // If queue not full
+            if (rear - front + 1 < n && rear != n - 1)
+            {
+                enqueue(q, &front, &rear, page, n);
+            }
+            else
+            {
+                dequeue(q, &front, &rear);
+                enqueue(q, &front, &rear, page, n);
+            }
+
+            printf("Page Fault Frames: ");
+        }
+        else
+        {
+            printf("Page Hit Frames: ");
+        }
+
+        display(q, front, rear);
+    }
+
+    printf("\nTotal Page Faults = %d\n", pageFaults);
+    printf("Total Page Hits = %d\n", m - pageFaults);
+
+    return 0;
+}
